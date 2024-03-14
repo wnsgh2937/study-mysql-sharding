c19d4f3a8d80a8de9c70456c3171d7d234904330
kakaoent-leo <leo.ee@kakaoent.com>
2024-03-14 23:55:39 +0900

    init

---
+++ src/main/kotlin/org/example/studymysqlsharding/StudyMysqlShardingApplication.kt
@@ -0,0 +1,11 @@
+package org.example.studymysqlsharding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyMysqlShardingApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyMysqlShardingApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/studymysqlsharding/StudyMysqlShardingApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.studymysqlsharding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyMysqlShardingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
